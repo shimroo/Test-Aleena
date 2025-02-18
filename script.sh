da1aa9331f8b7ebc1179ca0332819fa9fa43bd86
shimroo <arafayb@gmail.com>
2025-02-18 14:43:16 +0500

    two before script

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ num_iterations=1200  # Change as needed
 number_of_pages=250
 wait_time=15
 
-for ((i=1; i<=num_iterations; i++))
+for ((i=0; i<=num_iterations; i++))
 do
     echo "Running iteration $i... running pages.py with $number_of_pages pages"
     python3 pages.py "$number_of_pages"
@@ -14,14 +14,14 @@ do
     sleep $wait_time
 
     # After every 10 iterations, commit and push to Git
-    if ((i % 10 == 0)); then
+    if ((i % 20 == 0)); then
         echo "Iteration $i: Committing and pushing to Git..."
 
         # Add all changes to staging
         git add .
 
         # Commit the changes
-        git commit -m "Iteration $i: Automating commit after $i iterations"
+        git commit -m "Iteration $i : Automatic commit"
 
         # Push the changes to the remote repository
         git push origin main
